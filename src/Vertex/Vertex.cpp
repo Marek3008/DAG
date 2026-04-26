af2b34977d560bd0f77ee1963b67fb2ad885a6ce
Marek3008 <banasmre@gmail.com>
2026-04-26 14:16:22 +0200

    pridany State kvoli prehladavaniu

--- src/Vertex/Vertex.cpp
+++ src/Vertex/Vertex.cpp
@@ -5,8 +5,16 @@ void Vertex::AddEdge(Vertex* vertex){
     this->edges.push_back(vertex);
 }
 
+void Vertex::SetState(Vertex::State s){
+    this->state = s;
+}
+
 int Vertex::GetValue() const {
     return this->value;
+}
+
+Vertex::State Vertex::GetState() const {
+    return this->state;
 }
 
 std::vector<Vertex*> Vertex::GetEdges() const {
--- src/Vertex/Vertex.hpp
+++ src/Vertex/Vertex.hpp
@@ -4,15 +4,23 @@
 
 
 class Vertex{
+    public:
+        enum State {UNVISITED, VISITING, VISITED};
+    
     private:
         int value;
+        State state;
         std::vector<Vertex*> edges;
 
     public:
-        Vertex(int value) : value(value) {}
+        Vertex(int value) : value(value), state(UNVISITED) {}
 
         void AddEdge(Vertex* vertex);
+        void SetState(State s);
+
         int GetValue() const;
+        State GetState() const;
         std::vector<Vertex*> GetEdges() const;
+        
         void PrintEdges() const;
 };
